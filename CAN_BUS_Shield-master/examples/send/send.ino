2f6a616b0781871a904517db2786a6a437dcb48a
Le Forestier Romain <romain.le.forestier.inbox@gmail.com>
2015-06-04 13:13:32 +0200

    test lib can

--- CAN_BUS_Shield-master/examples/send/send.ino
+++ CAN_BUS_Shield-master/examples/send/send.ino
@@ -1,27 +1,36 @@
 // demo: CAN-BUS Shield, send data
-#include <mcp_can.h>
+#include "mcp_can.h"
 #include <SPI.h>
 
 // the cs pin of the version after v1.1 is default to D9
 // v0.9b and v1.0 is default D10
 const int SPI_CS_PIN = 9;
+const int led = 13;
+boolean state = false;
 
 MCP_CAN CAN(SPI_CS_PIN);                                    // Set CS pin
 
 void setup()
 {
-    Serial.begin(115200);
-
+    //Serial.begin(9600);
+    
+  //set the gps signal led
+  pinMode(led, OUTPUT);
+  digitalWrite(led, LOW);
+  
 START_INIT:
 
     if(CAN_OK == CAN.begin(CAN_500KBPS))                   // init can bus : baudrate = 500k
     {
-        Serial.println("CAN BUS Shield init ok!");
+       // Serial.println("CAN BUS Shield init ok!");
+        digitalWrite(led, HIGH);
     }
     else
     {
-        Serial.println("CAN BUS Shield init fail");
-        Serial.println("Init CAN BUS Shield again");
+        //Serial.println("CAN BUS Shield init fail");
+        //Serial.println("Init CAN BUS Shield again");
+        state = !state;
+        digitalWrite(led, (state ? HIGH : LOW));
         delay(100);
         goto START_INIT;
     }
@@ -32,7 +41,10 @@ void loop()
 {
     // send data:  id = 0x00, standrad frame, data len = 8, stmp: data buf
     CAN.sendMsgBuf(0x00, 0, 8, stmp);
-    delay(100);                       // send data per 100ms
+    
+    state = !state;
+    digitalWrite(led, (state ? HIGH : LOW));
+    delay(250);                       // send data per 250ms
 }
 
 /*********************************************************************************************************
